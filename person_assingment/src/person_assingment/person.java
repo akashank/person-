6082613d273eb237b0ca7534a39773be087862c6
akashank <41520285+akashank@users.noreply.github.com>
2018-08-06 18:15:34 +0530

    Add files via upload

---
+++ person_assingment/src/person_assingment/person.java
@@ -0,0 +1,10 @@
+package person_assingment;
+
+public class person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ person_assingment/src/person_assingment/persontest.java
@@ -0,0 +1,10 @@
+package person_assingment;
+
+public class persontest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
